50f6e6909d81714cbbabd886a6c5e10895148dd7
chang <shiyixinyang@163.com>
2020-08-18 15:10:52 +0800

    a

--- src/main/java/com/example/demo/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.po.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Integer> {
-}
